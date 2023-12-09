25cf81a9c914cf7dc9c1a5edde95a4df9c0e90d1
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2023-12-09 20:52:02 +0200

    Set the default size and enable to change the size

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 let gridContainer = document.querySelector('.inner-grid-container');
 const inputSize = document.querySelector('#size-range');
 let sizeValue = document.querySelector('.size-value');
-sizeValue.innerHTML = `${inputSize.value} x ${inputSize.value}` 
+sizeValue.innerHTML = `${inputSize.value} x ${inputSize.value}`; 
+makeGrid();
 
 inputSize.addEventListener('input', () => {
-    sizeValue.innerHTML = `${inputSize.value} x ${inputSize.value}`
-    
+    sizeValue.innerHTML = `${inputSize.value} x ${inputSize.value}`;
 
+    let elements = document.querySelectorAll('.element');
+    elements.forEach(element => gridContainer.removeChild(element));
+   
+    makeGrid(inputSize.value);
+});
+
+function makeGrid(size) {
+    if (size === undefined) {
+        size = 16;
+    }
     outer:
-    for (let i = 0; i < inputSize.value; i++) {
-        let rowCount = inputSize.value;
-        for (let j = inputSize.value; j < inputSize.value + 1; j) {
+    for (let i = 0; i < size; i++) {
+        let rowCount = size;
+        for (let j = size; j < size + 1; j) {
             if (rowCount == 0) {
                 continue outer;
             }
             let elem = document.createElement('div')
             elem.className = 'element';
-            elem.style.width = `${100/inputSize.value}%`;
-            elem.style.paddingBottom = `${100/inputSize.value}%`;
+            elem.style.width = `${100/size}%`;
+            elem.style.paddingBottom = `${100/size}%`;
             gridContainer.appendChild(elem);
             rowCount--;
         }
     }
-});
+}
 
 
 
